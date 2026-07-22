afd02f6d22809eec67e544c428a4f822291e9bdb
Naman-bangari <namanbangari94565@gmail.com>
2026-07-23 01:50:07 +0530

    input logic fix

--- src/com/robot/Main.java
+++ src/com/robot/Main.java
@@ -13,39 +13,36 @@ import com.robot.strategy.walk.NormalWalk;
 import com.robot.strategy.walk.Notwalk;
 import com.robot.strategy.walk.Walkable;
 
-class Main{
+class Main {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("please select number \n 1 Walk \n 2 No Walk\n");
-        Integer walk=sc.nextInt();
+        Integer walk = sc.nextInt();
         Walkable walkable;
-        if(walk.equals(1))
-            walkable=new NormalWalk();
-        else 
-            walkable=new Notwalk();
-        
+        if (walk.equals(1))
+            walkable = new NormalWalk();
+        else
+            walkable = new Notwalk();
 
-        System.out.println("please select number \n 1 Talk \n 2 No Falk\n");
-        Integer talk=sc.nextInt();
+        System.out.println("\nplease select number \n 1 Talk \n 2 No Talk\n");
+        Integer talk = sc.nextInt();
         Talkable talkable;
-        if(walk.equals(1))
-            talkable=new NormalTalk();
-        else 
-            talkable=new NotTalk();
+        if (talk.equals(1))
+            talkable = new NormalTalk();
+        else
+            talkable = new NotTalk();
 
-
-        System.out.println("please select number \n 1 Fly \n 2 No Fly\n");
-        Integer fly=sc.nextInt();
+        System.out.println("\nplease select number \n 1 Fly \n 2 No Fly\n");
+        Integer fly = sc.nextInt();
         Flyable flyable;
-        if(walk.equals(1))
-            flyable=new NormalFly();
-        else 
-            flyable=new NotFly();
+        if (fly.equals(1))
+            flyable = new NormalFly();
+        else
+            flyable = new NotFly();
 
-
-        Robot robot=new Robot(walkable,talkable,flyable);
+        Robot robot = new Robot(walkable, talkable, flyable);
         robot.allfunctionality();
 
     }
